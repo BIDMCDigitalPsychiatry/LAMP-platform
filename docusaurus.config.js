ab701fac3d43f78804de734eeff27055025ae86e
Aditya Vaidyam <avaidyam@bidmc.harvard.edu>
2021-05-25 10:04:52 -0400

    Remove unnecessary config file

--- docusaurus.config.js
+++
@@ -1,3 +0,0 @@
-module.exports = require('./package.json').docusaurus; 
-// quick hack to make relative file paths work correctly for plugins
-module.exports.plugins[0][1].openapiPath = require.resolve(module.exports.plugins[0][1].openapiPath); 
