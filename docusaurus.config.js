3b4ab71c3b839f56a98f855bc93232915a68ca95
ITHQ1947\saritha.pillai <saritha.pillai@zcocorporation.com>
2024-11-04 21:15:47 +0530

    Trying fixes

--- docusaurus.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = require('./package.json').docusaurus
-
-    // module.exports = { 
-    //   "baseUrl": "/",
-    //   "title": "LAMP Platform",
-    //   "url": "https://docs.lamp.digital",
-    // }
-  
